7e9d122bab630bce4da7723e7ffcd2f20bef48a5
mrjamiebowman <mrjamiebowman@gmail.com>
2023-06-23 22:22:21 -0500

    initial creation

---
+++ src/MrJB.OPA.StyraDas.API/Program.cs
@@ -0,0 +1,17 @@
+using MrJB.OPA.StyraDas.API;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+var app = builder.Build();
+
+var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
