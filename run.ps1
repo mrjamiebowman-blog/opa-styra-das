a925f76053d8b3f1606f33f2caf8f3b20d6ca1ed
mrjamiebowman <mrjamiebowman@gmail.com>
2023-07-03 15:11:44 -0500

    run, http, docker not working

---
+++ run.ps1
@@ -0,0 +1,6 @@
+clear
+
+# variables
+$VERSION = 'latest'
+
+docker run --rm -it mrjamiebowman/opa-styra-das:$VERSION
--- src/MrJB.OPA.StyraDas.API/Program.cs
+++ src/MrJB.OPA.StyraDas.API/Program.cs
@@ -3,10 +3,10 @@ var builder = WebApplication.CreateSlimBuilder(args);
 var app = builder.Build();
 
 var securityApi = app.MapGroup("/api/security/grid");
-securityApi.MapGet("/", () => Results.Ok("Access Granted Security Grid!"));
+securityApi.MapGet("/", () => Results.Ok("Access Granted: Security Grid!"));
 
 var systemApi = app.MapGroup("api/system/test");
-systemApi.MapGet("/", () => Results.Ok("Access Granted to System Test!"));
+systemApi.MapGet("/", () => Results.Ok("Access Granted: System Test!"));
 
 var upApi = app.MapGroup("api/up");
 upApi.MapGet("/", () => Results.Ok("UP"));
