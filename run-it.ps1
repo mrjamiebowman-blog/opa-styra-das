6403955fff3a3c04b19ffd99f2bd1c5384f90ab8
mrjamiebowman <mrjamiebowman@gmail.com>
2023-07-03 15:26:27 -0500

    docker working

--- run-it.ps1
+++ run-it.ps1
@@ -3,4 +3,9 @@ clear
 # variables
 $VERSION = 'latest'
 
-docker run --rm -it --entrypoint /bin/bash mrjamiebowman/opa-styra-das:$VERSION
+docker run --rm -it --name mrjb-opa-styra-das --entrypoint /bin/bash `
+    -e Logging__Loglevel__Default=Debug `
+    -e Logging__Loglevel__Microsoft.AspNetCore=Debug `
+    --name mrjb-opa-styra-das `
+    -p 8080:8080 `
+    mrjamiebowman/opa-styra-das:$VERSION
--- run.ps1
+++ run.ps1
@@ -3,4 +3,9 @@ clear
 # variables
 $VERSION = 'latest'
 
-docker run --rm -it mrjamiebowman/opa-styra-das:$VERSION
+docker run --rm -it `
+    -e Logging__Loglevel__Default=Debug `
+    -e Logging__Loglevel__Microsoft.AspNetCore=Debug `
+    --name mrjb-opa-styra-das `
+    -p 8080:8080 `
+    mrjamiebowman/opa-styra-das:$VERSION
